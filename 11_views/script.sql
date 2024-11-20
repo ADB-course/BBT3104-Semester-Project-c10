569265a9c11ad2727a3a057f34422754d1aa54d7
venus kabera <166646446+Ve3-nus@users.noreply.github.com>
2024-11-20 22:59:31 +0300

    views
    Signed-off-by: venus kabera <166646446+Ve3-nus@users.noreply.github.com>

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -1,1 +1,13 @@
+-- Create a view to get customer purchase history
+CREATE VIEW CustomerPurchaseHistory AS
+SELECT
+    c.Name AS CustomerName,
+    p.ProductName AS ProductName,
+    pu.Quantity AS QuantityPurchased,
+    pu.TotalPrice AS TotalPrice,
+    pu.PurchaseDate AS PurchaseDate
+FROM
+    Purchase pu
+JOIN Customer c ON pu.CustomerID = c.CustomerID
+JOIN Product p ON pu.ProductID = p.ProductID;
 -- Write your SQL code here
