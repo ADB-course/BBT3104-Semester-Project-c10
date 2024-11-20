2ddd8bc3269485704f8ed131616482d75ca0c745
venus kabera <166646446+Ve3-nus@users.noreply.github.com>
2024-11-20 22:59:09 +0300

    stored routines as functions
    Signed-off-by: venus kabera <166646446+Ve3-nus@users.noreply.github.com>

--- 10_stored_routines_as_functions/script.sql
+++ 10_stored_routines_as_functions/script.sql
@@ -1,1 +1,16 @@
+DELIMITER //
+
+-- Function to calculate total sales for a product
+CREATE FUNCTION TotalSales(p_ProductID INT)
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE total DECIMAL(10, 2);
+    SELECT SUM(Price * Quantity) INTO total
+    FROM Purchase
+    WHERE ProductID = p_ProductID;
+    RETURN IFNULL(total, 0);
+END //
+
+DELIMITER ;
 -- Write your SQL code here
